2615d820230b571b7eb69384960f7f6720d97632
nohailaamo <nohailaamo@gmail.com>
2025-12-16 21:40:07 +0100

    TP3

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
+import keycloak from './KeycloakService';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+keycloak.init({ onLoad: 'login-required' }).then(authenticated => {
+    if (authenticated) {
+        ReactDOM.render(<App />, document.getElementById('root'));
+    } else {
+        window.location.reload();
+    }
+});
